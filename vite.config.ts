b8909249ad2162a9178c264121a8e5922ef2ad6e
Laurynas Grigutis <laurynasgr@gmail.com>
2025-10-05 16:13:07 +0300

    Only build `dev-loader` in development mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,26 @@
 import replace from '@rollup/plugin-replace';
-import { defineConfig } from "vite";
+import { BuildEnvironmentOptions, defineConfig, UserConfig } from "vite";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 console.log({ 'process.env.NODE_ENV': process.env.NODE_ENV });
 
+const isDev = process.env.NODE_ENV === 'development';
+const input: NonNullable<BuildEnvironmentOptions['rollupOptions']>['input'] = {
+  'go-hass-cards': "./src/index.ts",
+}
+
+if (isDev) {
+  input['dev-loader'] = "./src/dev-loader.ts";
+}
+
 export default defineConfig({
   build: {
     outDir: "./dist",
     emptyOutDir: true,
     rollupOptions: {
-      input: {
-        'dev-loader': "./src/dev-loader.ts",
-        'go-hass-cards': "./src/index.ts",
-      },
+      input,
       output: {
         entryFileNames: (chunkInfo) => {
           return chunkInfo.name === 'dev-loader' ? 'dev-loader.js' : 'go-hass-cards.js';
@@ -24,7 +30,7 @@ export default defineConfig({
         replace({
           '__DEV_URL__': JSON.stringify(process.env.DEV_URL),
           '__HA_URL__': JSON.stringify(process.env.HA_URL),
-          'isDev': process.env.NODE_ENV === 'development',
+          'isDev': isDev,
         }),
       ]
     },
