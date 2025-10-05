2f0911eab49215d9e2b3ff6e7d80b4fd2efd745e
Laurynas Grigutis <laurynasgr@gmail.com>
2025-10-05 16:55:48 +0300

    Added logic to create a release tag

---
+++ scripts/check-release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+git fetch --tags
+
+release_version=$(jq -r '.version' < package.json)
+release_exists=$(git tag --list $release_version)
+
+echo "releaseVersion=$release_version" >> $GITHUB_OUTPUT
+
+if [ -z "$release_exists" ]; then
+    echo "Release $release_version does not exist. Creating it..."
+    echo "releaseExists=false" >> $GITHUB_OUTPUT
+else
+    echo "Release $release_version exists"
+    echo "releaseExists=true" >> $GITHUB_OUTPUT
+fi
