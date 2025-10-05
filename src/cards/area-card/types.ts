fafeee76c44d0e17da052b34b9c775b41f65c6b7
Laurynas Grigutis <laurynasgr@gmail.com>
2025-10-06 01:32:55 +0300

    Simplified top/side card types to be a direct list of `LovelaceCardConfig[]` instead of  `StackCardConfig`

--- src/cards/area-card/types.ts
+++ src/cards/area-card/types.ts
@@ -1,4 +1,3 @@
-import type { StackCardConfig } from "../../hass-types/panels/lovelace/cards/types";
 import type { LovelaceCardConfig } from "../../types";
 import type { SensorType } from "../../utils/sensors";
 
@@ -6,6 +5,6 @@ export type AreaCardConfig = LovelaceCardConfig & {
   area: string;
   aspect_ratio?: string;
   sensor_classes?: SensorType[];
-  top_cards?: StackCardConfig;
-  side_cards?: StackCardConfig;
+  top_cards: LovelaceCardConfig[];
+  side_cards: LovelaceCardConfig[];
 }
